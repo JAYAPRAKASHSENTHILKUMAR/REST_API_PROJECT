a0a7c3670ae05636e50b38240a33d141e2860810
JAYAPRAKASHSENTHILKUMAR <108513977+JAYAPRAKASHSENTHILKUMAR@users.noreply.github.com>
2023-08-28 15:33:01 +0530

    Add files via upload

---
+++ Swagger/src/main/java/com/example/Controller/UsersController.java
@@ -0,0 +1,45 @@
+package com.example.Controller;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Model.Users;
+import com.example.Repository.UsersRepo;
+
+
+@RestController
+@RequestMapping("/")
+public class UsersController
+{
+	@Autowired
+	UsersRepo srepo;
+	@PostMapping()
+		public Users saveDetails(@RequestBody Users aj)
+		{
+			return  srepo.save(aj);
+		}
+	@GetMapping()
+	public List<Users>getDetails()
+	{
+		return srepo.findAll();
+	}
+	@DeleteMapping("/{id}")
+	public String deleteStudent(@PathVariable("id")int id)
+	{
+		srepo.deleteById((long) id);
+		
+		
+		return "The id "+id+" is deleted";
+	}
+	
+}
---
+++ Swagger/src/main/java/com/example/Repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package com.example.Repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.example.Model.Users;
+
+public interface UsersRepo extends JpaRepository<Users,Long>{
+
+}
